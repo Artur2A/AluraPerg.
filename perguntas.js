7683201e960eeaf81997f296f270f2739a53dd09
Artur2A <00001105669403sp@al.educacao.sp.gov.br>
2024-10-25 13:18:37 -0300

    B

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Sri Lanka?',
+    'A capital da Sri Lanka é Colombo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Ovelha em Ingles?',
+    'Ovelha em ingles é Sheep'
+)
